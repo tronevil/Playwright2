2d63b633d7f9a020cf36c15750bf17fba1d371b6
Huy Tron <35919807+tronevil@users.noreply.github.com>
2025-05-06 15:58:03 +0700

    add booking spec file

--- tests/booking.spec.ts
+++ tests/booking.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import {Destination} from '../pageObjects/bookingPage.json';
+import {Destination,Reserve} from '../pageObjects/bookingPage.json';
 
 test('Navigate to Booking page', async ({ page }) => {
   //Navigate to Booking page
@@ -9,8 +9,21 @@ test('Navigate to Booking page', async ({ page }) => {
   await expect(page).toHaveTitle('BlazeDemo');
   //choose  Departure ciity
   await page.locator(Destination.departure_city).selectOption('Paris');
-  await page.locator(Destination.destination_city).selectOption('Rome')
+  await page.locator(Destination.destination_city).selectOption('Rome');
   await page.locator(Destination.findFlight_btn).click();
+
+  //CHECK RESULT 
+  //Verify URL is change to reserve
+  await expect(page).toHaveURL('https://blazedemo.com/reserve.php');
+  //Verify page display a list of corresponding flight
+  await expect(page.locator(Reserve.heading)).toContainText(['Paris to Rome']);
+  //Verify data in the table 
+  const table = page.locator('.table');
+  const rows = table.locator('tbody tr');
+  const rowCount = await rows.count();
+  console.log(`Rows count: ${rowCount}`);
+  await expect(rowCount).toBeGreaterThan(4);
+
 });
 
 
